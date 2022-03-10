8ce04e1e4a73f8ebd741bc8a9a36e76102b89587
Rodrigo Moreira <rodrigomoreiradasilva.of@gmail.com>
2022-03-10 13:43:11 -0300

    11 | Integrate registration with FIrebase

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,41 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import ColecaoCliente from "../backend/db/ColecaoCliente";
 import Botao from "../components/Botao";
 import Formulario from "../components/Formulario";
 import Layout from "../components/Layout";
 import Tabela from "../components/Tabela";
 import Cliente from "../core/Cliente";
+import ClienteRepositorio from "../core/ClienteRepositorio";
 
 export default function Home() {
-  const [ cliente, setCliente ] = useState<Cliente>(Cliente.vazio());
-  const [ visivel, setVisivel ] = useState<'tabela' | 'form'>('tabela');
+  const repo: ClienteRepositorio = new ColecaoCliente();
 
-  const clientes = [
-    new Cliente('Ana', 34, '1'),
-    new Cliente('Bia', 45, '2'),
-    new Cliente('Jefferson', 19, '3'),
-    new Cliente('Pedro', 21, '4'),
-    new Cliente('Clara', 23, '5')
-  ]
+  const [ cliente, setCliente ] = useState<Cliente>(Cliente.vazio());
+  const [ clientes, setClientes ] = useState<Cliente[]>([]);
+  const [ visivel, setVisivel ] = useState<'tabela' | 'form'>('tabela');
   
+  useEffect(obterTodos, [  ])
+  
+  function obterTodos() {
+    repo.obterTodos().then(clientes => {
+      setClientes(clientes)
+      setVisivel('tabela');
+    });
+  }
+
   function clienteSelecionado(cliente: Cliente) {
     setCliente(cliente);
     setVisivel('form');
   }
 
-  function clienteExcluido(cliente: Cliente) {
-    console.log('Usuário excluido' + cliente.nome);
+  async function clienteExcluido(cliente: Cliente) {
+    await repo.excluir(cliente);
+    obterTodos();
   }
 
-  function salvarCliente(cliente: Cliente) {
-    console.log(cliente);
-    setVisivel('tabela')
+  async function salvarCliente(cliente: Cliente) {
+    await repo.salvar(cliente);
+    obterTodos();
   }
 
   function novoCliente() {
